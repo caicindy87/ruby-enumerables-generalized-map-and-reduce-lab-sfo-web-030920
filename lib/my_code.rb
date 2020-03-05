24d0e476c814f35194fd72c0bd9d95326fec7f14
Cindy Cai <caicindy87@gmail.com>
2020-03-05 00:30:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  index = 0
+  while index < array.length do
+    new_array.push(yield(array[index]))
+    index += 1
+  end
+  new_array
+end
+
+def reduce(array, starting_value = 0)
+  total = 0 + starting_value
+  index = 0
+  while index < array.length do
+    total += yield(array[index] + starting_value)
+    index += 1
+  end
+  total
+end
+
+
